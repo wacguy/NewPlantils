8d91ce848c7367a2a95b1bb847b853c9423e5ced
daiane <daianeasilva26@gmail.com>
2022-02-19 20:35:35 -0500

    Guys photo added

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,10 @@
 // Scripts
 // 
 
+const contactDiv = document.querySelector(".contact");
+const contactForm = document.getElementById('contact-form');
+const message = document.querySelector('.message');
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -52,3 +56,52 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// Send message through form
+
+(function() {
+    emailjs.init("user_sOKqZKmVqQDVPfCQ7jueQ");
+  })();
+  
+  window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // Send form information to email JS api.
+        emailjs.sendForm('service_2zfeihg', 'template_lhf9757', 'contact-form')
+            .then(function() {
+              sucessfulMessage();
+            }, function(error) {
+              failureMessage();
+            });
+    });
+  }
+  
+  //Successful Message
+  function sucessfulMessage(){
+    contactForm.hidden = true;
+    message.textContent = 'Thank you for your message. We will be in contact with you soon!';
+    timer = setInterval(restoreFormSuccess, 2000);
+  }
+  
+  //Failure Message 
+  function failureMessage() {
+    contactForm.hidden = true;
+    message.textContent = "Ooops! Your message wasn't sent. Please try again!"
+    timer = setInterval(restoreFormFail, 2000);
+  }
+  
+  //Restore form Success
+  function restoreFormSuccess() {
+    message.textContent = 'Get in touch!'
+    contactForm.hidden = false;
+    document.getElementById('contact-form').reset();
+  }
+  
+  //Restore form Failure
+  function restoreFormFail() {
+    message.textContent = 'Get in touch!'
+    contactForm.hidden = false;
+  
+  }
